c8b56730e46d2cb4909c005b4a146b4059cd313c
Jamie <jmeshen@gmail.com>
2015-05-13 12:08:51 -0400

    index route complete

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+swig.setDefaults({cache: false});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+var models = require('../models');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.Hotel.find({}, function(err, hotels) {
+    models.Restaurant.find({}, function(err, restaurants) {
+      models.ThingToDo.find({}, function(err, thingsToDo) {
+        console.log('HOTELS: ', hotels);
+        console.log('RESTAURANTS: ', restaurants);
+        console.log('THINGSTODO: ', thingsToDo);
+        res.render('index', {
+
+          all_hotels: hotels,
+          all_restaurants: restaurants,
+          all_things_to_do: thingsToDo
+        })
+      })
+    })
+  })
+
 });
 
 module.exports = router;
